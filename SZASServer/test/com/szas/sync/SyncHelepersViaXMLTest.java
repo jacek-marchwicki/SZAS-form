83be2fefb2288899a741b321505b605bfb9d6228
Jacek Marchwicki <jacek@3made.eu>
2011-03-31 00:56:47 +0200

    Test for XMLSerializer-Deserializer

---
+++ SZASServer/test/com/szas/sync/SyncHelepersViaXMLTest.java
@@ -0,0 +1,72 @@
+package com.szas.sync;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import javax.xml.bind.JAXBException;
+
+import com.szas.sync.local.SyncLocalService;
+import com.szas.sync.local.SyncLocalServiceResult;
+import com.thoughtworks.xstream.XStream;
+
+public class SyncHelepersViaXMLTest extends SyncHelpersTest {
+	@Override
+	protected SyncLocalService getSyncLocalService() {
+		return new SyncLocalService() {
+			
+			@Override
+			public void sync(ArrayList<ToSyncElementsHolder> toSyncElementsHolders,
+					SyncLocalServiceResult callback) {
+				
+				
+				
+				try {
+					// CLient side
+					String toServer;
+					XStream xStream = new XStream();
+					toServer = xStream.toXML(toSyncElementsHolders);
+					
+					
+					// Server side
+					String toClient = serverSide(toServer);	
+					
+					
+					// Client side
+					@SuppressWarnings("unchecked")
+					ArrayList<SyncedElementsHolder> result = 
+						(ArrayList<SyncedElementsHolder>) xStream.fromXML(toClient);
+					
+					callback.onSuccess(result);
+				} catch (WrongObjectThrowable e) {
+					callback.onFailure(e);
+				} catch (JAXBException e) {
+					e.printStackTrace();
+					callback.onFailure(e);
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+					callback.onFailure(e);
+				} catch (ClassCastException e) {
+					callback.onFailure(e);
+					e.printStackTrace();
+				}
+			}
+
+			private String serverSide(String toServer) throws JAXBException,
+					UnsupportedEncodingException, WrongObjectThrowable, ClassCastException {
+				XStream xStream = new XStream();
+
+				@SuppressWarnings("unchecked")
+				ArrayList<ToSyncElementsHolder> toSyncElementsHolders = 
+					(ArrayList<ToSyncElementsHolder>) xStream.fromXML(toServer);
+				
+				ArrayList<SyncedElementsHolder> server_sResult =
+					remoteSyncHelper.sync(toSyncElementsHolders);
+				
+				
+				String toClient = xStream.toXML(server_sResult);
+				
+				return toClient;
+			}
+		};
+	}
+}
