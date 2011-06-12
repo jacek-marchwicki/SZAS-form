7b4cbf2a883828a12ea249ca9e6ced966d4e6a77
pszafer <pszafer@gmail.com>
2011-06-12 03:10:02 +0200

    gzipping is working!

--- SZASAndroid/src/com/szas/android/SZASApplication/SyncAdapter.java
+++ SZASAndroid/src/com/szas/android/SZASApplication/SyncAdapter.java
@@ -11,6 +11,7 @@ import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 import org.apache.http.client.ClientProtocolException;
@@ -55,18 +56,20 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 	private static final String LOGTAG = "SZAS_SYNC_ADAPTER";
 
 	private final AccountManager accountManager;
-	
+
 	boolean isChanged = false;
-	
-	private final static class AndroidSyncLocalService implements SyncLocalService {
-		
+
+	private final static class AndroidSyncLocalService implements
+			SyncLocalService {
+
 		/**
-		 * @param googleAuthentication 
+		 * @param googleAuthentication
 		 * 
 		 */
 		public AndroidSyncLocalService(GoogleAuthentication googleAuthentication) {
 			this.googleAuthentication = googleAuthentication;
 		}
+
 		/**
 		 * GAE URL
 		 */
@@ -75,18 +78,22 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 		/**
 		 * GAE sync URL
 		 */
-		//String gaeSyncUrl = gaeUrl + "sync?oauth_token=";
+		// String gaeSyncUrl = gaeUrl + "sync?oauth_token=";
 		GoogleAuthentication googleAuthentication;
 
-		/* (non-Javadoc)
-		 * @see com.szas.sync.local.SyncLocalService#sync(java.util.ArrayList, com.szas.sync.local.SyncLocalServiceResult)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see com.szas.sync.local.SyncLocalService#sync(java.util.ArrayList,
+		 * com.szas.sync.local.SyncLocalServiceResult)
 		 */
 		@Override
 		public void sync(ArrayList<ToSyncElementsHolder> toSyncElementsHolders,
 				SyncLocalServiceResult callback) {
-			
+
 			try {
-				ArrayList<SyncedElementsHolder> elements = fetchFromNetwork(toSyncElementsHolders, googleAuthentication);
+				ArrayList<SyncedElementsHolder> elements = fetchFromNetwork(
+						toSyncElementsHolders, googleAuthentication);
 				Log.v("Sync", "Callback.onSuccess");
 				callback.onSuccess(elements);
 			} catch (ClientProtocolException e1) {
@@ -98,7 +105,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 			}
 
 		}
-		
+
 		/**
 		 * Method to download syncing elements from network
 		 * 
@@ -114,39 +121,58 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 				throws ClientProtocolException, IOException {
 			URL url = new URL(gaeUrl + "sync");
 			URLConnection conn = url.openConnection();
-			conn.addRequestProperty("Content-Type", "application/json; charset=UTF-8");
-			conn.addRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
+			conn.addRequestProperty("Content-Type",
+					"application/json; charset=UTF-8");
 			conn.addRequestProperty("Content-Encoding", "gzip");
-			conn.addRequestProperty("Cookie", googleAuthentication.getAuthCookie()
-					.getName()
+			conn.addRequestProperty("Cookie", googleAuthentication
+					.getAuthCookie().getName()
 					+ "="
 					+ googleAuthentication.getAuthCookie().getValue());
-			conn.addRequestProperty("Charset", "UTF-8");
-			//this do post method
-			conn.setDoOutput(true);		
+			conn.addRequestProperty("User-Agent",
+					"Mozilla/5.0 (X11; Linux x86_64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1");
+			conn.addRequestProperty("Accept",
+					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			conn.addRequestProperty("Accept-Language",
+					"pl,en-us;q=0.7,en;q=0.3");
+			conn.addRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
+			conn.addRequestProperty("Accept-Charset", "utf-8;q=0.7,*;q=0.7");
+			conn.addRequestProperty("Cache-Control", "no-cache");
+			conn.addRequestProperty("Pragma", "no-cache");
+			// this do post method
+			conn.setDoOutput(true);
 			OutputStream outputStream = conn.getOutputStream();
-			GZIPOutputStream gzipOutputStream = new GZIPOutputStream(outputStream);
+			GZIPOutputStream gzipOutputStream = new GZIPOutputStream(
+					outputStream);
 			OutputStreamWriter out = new OutputStreamWriter(gzipOutputStream);
-			new JSONSerializer().prettyPrint(true).include("*").serialize(elementsToSync, out);
+			new JSONSerializer().prettyPrint(true).include("*")
+					.serialize(elementsToSync, out);
 			out.close();
 			gzipOutputStream.close();
 			InputStream inputStream = null;
 			inputStream = conn.getInputStream();
 			String encoding = conn.getContentEncoding();
-			 if (encoding == null) {
-				    encoding = "UTF-8";
-				  }
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, encoding);
-			if (inputStream == null) 
+			String charset = conn.getContentType();
+			int startIndex = charset.indexOf("charset=") + 8;
+			String word = charset.substring(startIndex).split(" |\"\"")[0];
+			if (word == null) {
+				word = "utf-8";
+			}
+			InputStreamReader inputStreamReader;
+			if (inputStream == null)
 				return null;
-
-			ArrayList<SyncedElementsHolder> result =
-				new JSONDeserializer<ArrayList<SyncedElementsHolder>>().deserialize(inputStreamReader);
+			if (encoding.toLowerCase().equals("gzip")) {
+				GZIPInputStream gzipInputStream = new GZIPInputStream(
+						inputStream);
+				inputStreamReader = new InputStreamReader(gzipInputStream, word);
+			} else {
+				inputStreamReader = new InputStreamReader(inputStream, word);
+			}
+			ArrayList<SyncedElementsHolder> result = new JSONDeserializer<ArrayList<SyncedElementsHolder>>()
+					.deserialize(inputStreamReader);
 			return result;
 		}
-		
+
 	}
-	
 
 	private LocalDAO<QuestionnaireTuple> questionnaireDAO;
 
@@ -168,13 +194,15 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 		super(context, autoInitialize);
 		this.context = context;
 		this.accountManager = AccountManager.get(context);
-		questionnaireDAO = new SQLLocalDAO<QuestionnaireTuple>(context, "com.szas.data.QuestionnaireTuple");
-		filledQuestionnaireDAO = new SQLLocalDAO<FilledQuestionnaireTuple>(context, "com.szas.data.FilledQuestionnaireTuple");
-		if(questionnaireDAO.getAll().isEmpty())
+		questionnaireDAO = new SQLLocalDAO<QuestionnaireTuple>(context,
+				"com.szas.data.QuestionnaireTuple");
+		filledQuestionnaireDAO = new SQLLocalDAO<FilledQuestionnaireTuple>(
+				context, "com.szas.data.FilledQuestionnaireTuple");
+		if (questionnaireDAO.getAll().isEmpty())
 			questionnaireDAO.setLastTimestamp(-1);
-		if(filledQuestionnaireDAO.getAll().isEmpty())
+		if (filledQuestionnaireDAO.getAll().isEmpty())
 			filledQuestionnaireDAO.setLastTimestamp(-1);
-	
+
 	}
 
 	/*
@@ -188,30 +216,31 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 	@Override
 	public void onPerformSync(Account account, Bundle extras, String authority,
 			ContentProviderClient contentProviderClient, SyncResult syncResult) {
-		
+
 		boolean bool = extras.getBoolean(ContentResolver.SYNC_EXTRAS_MANUAL);
-		if(bool){
+		if (bool) {
 			Log.v(LOGTAG, "syncAdapter sync started manually");
-		}else{
+		} else {
 			Log.v(LOGTAG, "syncAdapter sync started automatically");
 		}
-		GoogleAuthentication googleAuthentication = GoogleAuthentication.getGoogleAuthentication(account);
-		if(!googleAuthentication.connect(accountManager)){
+		GoogleAuthentication googleAuthentication = GoogleAuthentication
+				.getGoogleAuthentication(account);
+		if (!googleAuthentication.connect(accountManager)) {
 			syncResult.stats.numAuthExceptions++;
 			return;
 		}
-		if (googleAuthentication.getAuthCookie() == null)
-		{
+		if (googleAuthentication.getAuthCookie() == null) {
 			syncResult.stats.numAuthExceptions++;
 			return;
 		}
 		syncLocalService = new AndroidSyncLocalService(googleAuthentication);
-		LocalSyncHelperImpl syncHelper = new LocalSyncHelperImpl(syncLocalService);
+		LocalSyncHelperImpl syncHelper = new LocalSyncHelperImpl(
+				syncLocalService);
 		syncHelper.append("questionnaire", getQuestionnaireDAO());
 		syncHelper.append("filled", filledQuestionnaireDAO);
 		registerContentObservers();
 		syncHelper.sync();
-		if(isChanged){
+		if (isChanged) {
 			sendMessage("info");
 			isChanged = false;
 		}
@@ -219,9 +248,11 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 	}
 
 	/**
-	 * @param questionnaireDAO the questionnaireDAO to set
+	 * @param questionnaireDAO
+	 *            the questionnaireDAO to set
 	 */
-	public void setQuestionnaireDAO(LocalDAO<QuestionnaireTuple> questionnaireDAO) {
+	public void setQuestionnaireDAO(
+			LocalDAO<QuestionnaireTuple> questionnaireDAO) {
 		this.questionnaireDAO = questionnaireDAO;
 	}
 
@@ -231,13 +262,13 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 	public LocalDAO<QuestionnaireTuple> getQuestionnaireDAO() {
 		return questionnaireDAO;
 	}
-	
-	public void sendMessage(String information){
+
+	public void sendMessage(String information) {
 		Intent i = new Intent(Constans.broadcastMessage);
 		i.putExtra("info", information);
 		context.sendBroadcast(i);
 	}
-	
+
 	private void registerContentObservers() {
 		ContentResolver cr = context.getContentResolver();
 		cr.registerContentObserver(
@@ -252,8 +283,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 		}
 	}
 
-	private ContentObserver contentObserver = new ContentObserver(
-			new Handler()) {
+	private ContentObserver contentObserver = new ContentObserver(new Handler()) {
 		@Override
 		public void onChange(boolean selfChange) {
 			isChanged = true;
